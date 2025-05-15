48cab91e4e857d24668163a9ad664796f0f7f4fb
Zeineb Ben Abdallah <zeineb.benabdallah@insat.ucar.tn>
2025-05-15 17:46:17 +0100

    fix terraform

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_public_ip" "example" {
   name                = "jenkins-vm-pip"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Create network interface
